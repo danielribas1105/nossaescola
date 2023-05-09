4dc87920d5840d6fd537c0a45a9a919969938d1c
Daniel <danielribas1105@gmail.com>
2023-05-08 22:35:35 -0300

    update project react

---
+++ src/component/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner(){
+    //JSX
+    return (
+        <header className="banner">
+        <img src="/images/banner-nossa-escola.jpg" alt="Banner Nossa Escola"/>
+        </header>
+    );
+}
+
+export default Banner;
